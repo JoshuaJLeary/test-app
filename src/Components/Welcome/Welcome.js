bf563f2108af7400a0ce536896a9c0da7d997274
Joshua John <JoshuaLeary@Danicas-MacBook-Pro.local>
2018-10-11 17:19:49 -0500

    help

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -11,14 +11,15 @@ class Welcome extends Component {
   render() {
     const signUpDetails = this.props;
     // const state = signUpDetails;
-    console.log('info?', signUpDetails);
+    console.log('this is signUpDetails', signUpDetails);
     return (
       <div className="welcome">
         <h2>Welcome,</h2>
-        <h1>{name}</h1>
+        <h1>{signUpDetails.name}</h1>
         <p>You have been registered for this awesome service.
           Please check your email listed below for instructions.
         </p>
+        <h2>{signUpDetails.email}</h2>
       </div>
     );
   }
